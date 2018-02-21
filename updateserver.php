469189714bce0fcba3aadb770f1e1a0ea651a2a8
daan <daan43149036@gmail.com>
2018-02-21 12:09:40 +0000

    Create updateserver.php

---
+++ updateserver.php
@@ -0,0 +1,2 @@
+<?php header( "Status: 301 Moved Permanently" ); header( "Location: <?php header( "Status: 301 Moved Permanently" ); header( "Location: http://www.jouwsite.nl/" ); exit(0); // dit is optioneel, maar wordt wel aangeraden. Hiermee voorkom je onnodige output ?>
+" ); exit(0); // dit is optioneel, maar wordt wel aangeraden. Hiermee voorkom je onnodige output ?>
